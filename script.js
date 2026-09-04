e0532d50866dc780c739c593f8aad29613800269
testtarou-cell <digidegi.art@gmail.com>
2026-09-04 22:50:03 +0900

    Revert "`#moreButton`で`#message`の表示切替を行う`script.js`を追加"

--- script.js
+++
@@ -1,7 +0,0 @@
-const moreButton = document.querySelector('#moreButton');
-const message = document.querySelector('#message');
-
-moreButton.addEventListener('click', () => {
-  message.hidden = !message.hidden;
-  moreButton.textContent = message.hidden ? 'もっと見る' : '閉じる';
-});
